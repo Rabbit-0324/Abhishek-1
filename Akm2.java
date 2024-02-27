577214aa057444d98b17a2a9af48548ba2706ea1
Rabbit-0324 <161414740+Rabbit-0324@users.noreply.github.com>
2024-02-27 12:04:11 +0530

    Add files via upload

---
+++ Akm2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Akm2
+{
+  public static void main (String args[])
+{
+int a=150, b=150;
+System.out.println("Addition is"+ (a+b));
+System.out.println("Substraction is"+ (a-b));
+System.out.println("Multiplication is " + (a*b));
+System.out.println("Division is" + (a/b));
+  }
+}
