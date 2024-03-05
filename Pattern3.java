37acda60302080a7c2197afc2656584134927933
Rabbit-0324 <161414740+Rabbit-0324@users.noreply.github.com>
2024-03-05 11:52:45 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,19 @@
+class Pattern3
+{
+public static void main (String args[])
+{
+int i, j;
+for (i=5; i>=1; i--)
+{
+for (j=4; j>=i-1; j--)
+{
+System.out.print("*");
+}
+for (j=5; j>=i; j--)
+{
+System.out.print(" ");
+}
+System.out.println();
+}
+}
+}
