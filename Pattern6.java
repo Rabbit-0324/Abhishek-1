7d0e89d960c933e31ec5906f453ce7c6a04df8ea
Rabbit-0324 <161414740+Rabbit-0324@users.noreply.github.com>
2024-03-09 11:58:29 +0530

    Add files via upload

---
+++ Pattern6.java
@@ -0,0 +1,19 @@
+class Pattern6
+{
+public static void main (String args[])
+{
+int i, j;
+for (i=5; i>=0; i--)
+{
+for (j=1; j<=i+1; j++)
+{
+System.out.print("");
+}
+for (j=1; j<=i; j++)
+{
+System.out.print(" *");
+}
+System.out.println();
+}
+}
+}
